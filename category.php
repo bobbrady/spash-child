6d93fcd67c9ce9b266bce76cac8c7f41bb1d4969
Bob Brady <bob@digibrady.com>
2015-09-27 19:46:37 -0400

    Fixed category page queries for just sticky and non-sticky posts

--- category.php
+++ category.php
@@ -1,65 +1,63 @@
-  <?php $mts_options = get_option('splash'); ?>
-  <?php get_header(); ?>
-  <div id="page">
+<?php $mts_options = get_option('splash'); ?>
+<?php get_header(); ?>
+<div id="page">
   <div class="<?php mts_article_class(); ?>">
     <div id="content_box">
      <h1 class="postsby">
       <span><?php single_cat_title(); ?><?php _e(" Category", "mythemeshop"); ?></span>
     </h1>
-    <?php
-    $main_cat_ID = get_query_var( 'cat' );
+<?php
+// work out category id, then see if there are any subcategories
+$main_cat_ID = get_query_var( 'cat' );
+// if it is page 1
+if ( $paged < 2 ) :
+    // show the category description
+   echo category_description();
+   //  $args = array( 'category__in' => $main_cat_ID, 'include' => get_option( 'sticky_posts' ) );
 
-    // if it is page 1
-    if ( $paged < 2 ) :
-      echo category_description( $category_id );
-      // get sticky posts and show them if they exist
-      $args = array( 'category__in' => $main_cat_ID, 'include' => get_option( 'sticky_posts' ) );
-      $myposts = get_posts( $args );
-      if ( count( $myposts ) > 0 ) :
-    ?>
-        <h2>Featured Posts</h2>
-    <?php foreach ( $myposts as $post ) : setup_postdata( $post );
-        set_query_var( 'mts_options', $mts_options );
-        get_template_part( 'articlesummary');
-        endforeach;
-        endif;
-        else :
-    ?>
+   $args = array('post__in' => get_option( 'sticky_posts' ), 'ignore_sticky_posts' => 1, 'order' => 'ASC' ,
+   	'category__in' => $main_cat_ID);
+
+   query_posts( $args );
+
+   if (have_posts()) :
+?>
+   		<h2>Featured Posts</h2>
+<?php
+			// The Loop
+			while ( have_posts() ) : the_post();
+    		set_query_var( 'mts_options', $mts_options );
+      	get_template_part( 'articlesummary');
+			endwhile;
+		endif;
+
+// pages 2 and onwards don't worry about cat desc or stickies, but link to main cat page
+else : ?>
         <p>For an introduction to this topic and our latest articles, please see the main
         <a href="<?php echo get_category_link( $main_cat_ID ); ?>"><?php single_cat_title(); ?></a> page.</p>
-    <?php
-      endif;
-        wp_reset_postdata();
-        $selected_layout = (!empty($mts_options['mts_sorting']) && !empty($_COOKIE['selected_layout']) && $_COOKIE['selected_layout'] == 'grid' ? 'grid' : 'list');
-        if($mts_options['mts_sorting'] == '1') {
-    ?>
-          <div class="viewstyle">
-           <span class="viewtext"><?php _e('Show Posts in','mythemeshop'); ?></span>
-           <div class="viewsbox">
-            <div id="list" <?php echo ($selected_layout == 'list' ? 'class="active"' : ''); ?>><a><i class="fa fa-list"></i> <?php _e('List View','mythemeshop'); ?></a></div>
-            <div id="grid" <?php echo ($selected_layout == 'grid' ? 'class="active"' : ''); ?>><a><i class="fa fa-th"></i> <?php _e('Grid View','mythemeshop'); ?></a></div>
-          </div>
-          <div style="clear:both;"></div>
-        </div>
-        <?php } ?>
-        <h2>Recent Posts</h2>
-        <?php $j = 0; if (have_posts()) : while (have_posts()) : the_post();
-        set_query_var( 'mts_options', $mts_options );
+<?php
+endif;
+// IMPORTANT
+wp_reset_query();
+$j = 0; if (have_posts()) : ?>
+				<h2>Recent Posts</h2>
+			<?php while (have_posts()) : the_post();
+				set_query_var( 'mts_options', $mts_options );
         get_template_part( 'articlesummary');
-        endwhile; endif; ?>
-        <!--Start Pagination-->
-        <?php if ($mts_options['mts_pagenavigation'] == '1' ) { ?>
-        <?php  $additional_loop = 0; mts_pagination($additional_loop['max_num_pages']); ?>
-        <?php } else { ?>
-        <div class="pagination">
-         <ul>
-          <li class="nav-previous"><?php next_posts_link( '<i class="fa fa-angle-left"></i> '. __( 'Previous', 'mythemeshop' ) ); ?></li>
-          <li class="nav-next"><?php previous_posts_link( __( 'Next', 'mythemeshop' ).' <i class="fa fa-angle-right"></i>' ); ?></li>
-        </ul>
-      </div>
-      <?php } ?>
-      <!--End Pagination-->
-    </div>
-   </div>
-  <?php get_sidebar(); ?>
-  <?php get_footer(); ?>
+			endwhile; endif; ?>
+			<!--Start Pagination-->
+			<?php if ($mts_options['mts_pagenavigation'] == '1' ) { ?>
+				<?php  $additional_loop = 0; mts_pagination($additional_loop['max_num_pages']); ?>
+			<?php } else { ?>
+				<div class="pagination">
+					<ul>
+						<li class="nav-previous"><?php next_posts_link( '<i class="fa fa-angle-left"></i> '. __( 'Previous', 'mythemeshop' ) ); ?></li>
+						<li class="nav-next"><?php previous_posts_link( __( 'Next', 'mythemeshop' ).' <i class="fa fa-angle-right"></i>' ); ?></li>
+					</ul>
+				</div>
+			<?php } ?>
+			<!--End Pagination-->
+		</div>
+	</div>
+	<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -12,4 +12,20 @@ function removecategorydescrption($columns)
 }
 add_filter('manage_edit-category_columns','removecategorydescrption');
 
+// don't show stickies or posts from child categories on category pages
+function bradythink_category_posts( $query = false ) {
+
+    // Bail if not home, not a query, not main query, or if it's the admin area or a feed
+    if ( ! $query->is_category || ! is_a( $query, 'WP_Query' ) || ! $query->is_main_query() || $query->is_admin || $query->is_feed )
+        return;
+
+    // only get posts not in sticky posts
+    $query->set( 'post__not_in', get_option( 'sticky_posts' ) );
+
+    // only get posts in this category (not in child categories)
+    $query->set( 'category__in', get_category_by_slug( get_query_var( 'category_name' ) )->cat_ID );
+
+}
+add_action( 'pre_get_posts', 'bradythink_category_posts' );
+
 ?>
